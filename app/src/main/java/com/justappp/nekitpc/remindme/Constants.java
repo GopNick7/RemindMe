376a50a13972d9d4c1735493ab3567d196edfef6
Nikita <nekit88j@gmail.com>
2017-04-12 22:49:00 +0300

    implemented navigation view and tab navigation with test fragment

---
+++ app/src/main/java/com/justappp/nekitpc/remindme/Constants.java
@@ -0,0 +1,13 @@
+package com.justappp.nekitpc.remindme;
+
+/**
+ * Created by NEKIT PC on 12.04.2017.
+ */
+
+public class Constants {
+
+    public static final int TAB_ONE = 0;
+    public static final int TAB_TWO = 1;
+    public static final int TAB_THREE = 2;
+
+}
--- app/src/main/java/com/justappp/nekitpc/remindme/MainActivity.java
+++ app/src/main/java/com/justappp/nekitpc/remindme/MainActivity.java
@@ -1,15 +1,23 @@
 package com.justappp.nekitpc.remindme;
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
 import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 
+import com.justappp.nekitpc.remindme.adapter.TabsPagerFragmentAdapter;
+
 public class MainActivity extends AppCompatActivity {
 
     Toolbar toolbar;
     DrawerLayout drawerLayout;
+    ViewPager viewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +27,7 @@ public class MainActivity extends AppCompatActivity {
 
         initToolbar();
         initNavigationView();
+        initTabLayout();
     }
 
     private void initToolbar() {
@@ -35,5 +44,36 @@ public class MainActivity extends AppCompatActivity {
 
     private void initNavigationView() {
         drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.view_navigation_open, R.string.view_navigation_close);
+        drawerLayout.setDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.navigation);
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                drawerLayout.closeDrawers();
+                switch (item.getItemId()){
+                    case R.id.actionNotificationItem:
+                        showNotificationTab();
+                }
+                return true;
+            }
+        });
+    }
+
+    private void initTabLayout() {
+        viewPager = (ViewPager) findViewById(R.id.viewPager);
+        TabsPagerFragmentAdapter adapter = new TabsPagerFragmentAdapter(getSupportFragmentManager());
+        viewPager.setAdapter(adapter);
+
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.tab_layout);
+        tabLayout.setupWithViewPager(viewPager);
+
+    }
+
+    private void showNotificationTab(){
+        viewPager.setCurrentItem(Constants.TAB_TWO);
     }
 }
