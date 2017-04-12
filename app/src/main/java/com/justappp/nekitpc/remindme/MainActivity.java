2217216714ec2869e475d98b97c56f5794e967a1
Nikita <nekit88j@gmail.com>
2017-04-12 18:07:00 +0300

    Add NavigationView

--- app/src/main/java/com/justappp/nekitpc/remindme/MainActivity.java
+++ app/src/main/java/com/justappp/nekitpc/remindme/MainActivity.java
@@ -12,6 +12,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setTheme(R.style.AppDefault);
         setContentView(R.layout.activity_main);
 
         initToolbar();
